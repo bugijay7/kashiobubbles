5c85ea65c43590ddbe9a78e1f3e60491d548211b
Kibugi Maina <138654192+bugijay7@users.noreply.github.com>
2025-07-02 13:17:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://kashiobubbles.vercel.app",
   credentials: true
 }));
 app.use(express.urlencoded({ extended: true }));
